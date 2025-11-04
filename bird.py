9bbaab92e5087ebecf3ca8ac61a170491fe0e491
J1214 <horse121464@gmail.com>
2025-11-04 21:58:17 +0900

    프레임 위치 수정

--- bird.py
+++ bird.py
@@ -12,7 +12,7 @@ RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-TIME_PER_ACTION = 0.5
+TIME_PER_ACTION = 1.0
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 14
 
@@ -33,9 +33,19 @@ class Fly:
 
     def draw(self):
         if self.bird.face_dir == 1:
-            self.bird.image.clip_draw(int(self.bird.frame) * 183, 170, 150, 150, self.bird.x, self.bird.y)
+            if 0 <= self.bird.frame <= 4:
+                self.bird.image.clip_draw(int(self.bird.frame) * 181, 168*2, 170, 160, self.bird.x, self.bird.y)
+            elif 5 <= self.bird.frame <= 9:
+                self.bird.image.clip_draw(int(self.bird.frame-5) * 181, 168*1, 170, 160, self.bird.x, self.bird.y)
+            elif 10 <= self.bird.frame <= 13:
+                self.bird.image.clip_draw(int(self.bird.frame-10) * 181, 168*0, 170, 160, self.bird.x, self.bird.y)
         else:
-            self.bird.image.clip_composite_draw(int(self.bird.frame) * 183, 170, 150, 150, self.bird.x, self.bird.y)
+            if 0 <= self.bird.frame <= 4:
+                self.bird.image.clip_composite_draw(int(self.bird.frame) * 181, 168 * 2, 170, 160, self.bird.x, self.bird.y)
+            elif 5 <= self.bird.frame <= 9:
+                self.bird.image.clip_composite_draw(int(self.bird.frame - 5) * 181, 168 * 1, 170, 160, self.bird.x, self.bird.y)
+            elif 10 <= self.bird.frame <= 13:
+                self.bird.image.clip_composite_draw(int(self.bird.frame - 10) * 181, 168 * 0, 170, 160, self.bird.x, self.bird.y)
 
 
 
